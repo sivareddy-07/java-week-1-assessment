95ea338d8d5cac4a6c9f96f1ca5304a8fd70f34b
kothakapusivasankarreddy <kothakapusivashankarreddy@gmail.com>
2026-02-03 11:25:04 +0530

    first commit

--- task5.java
+++ task5.java
@@ -1,30 +1,26 @@
-import java.io.*;
-import java.util.*;
-class task5 {
-    public int countPrimesInRange(int input1, int input2) {
-        int d = input1;
-        int cou = 0;
-
-        while (d <= input2) {
-            int c = 0;
-
-            if (d < 2) {   // Numbers less than 2 are not prime
-                d++;
-                continue;
-            }
-            for (int i = 2; i < d; i++) {
-                if (d % i == 0) {
-                    c++;
-                    break;
-                }
-            }
-            if (c == 0) {
-                cou++;
-                System.out.println(d);
-            }
-            d++;
-        }
-        return cou;
-    }
+  public class task5
+ {
+ public int countPrimesInRange(int input1,int input2){
+ int d=input1,i,c=0; 
+ int cou=0; 
+ while(d<=input2){
+ for(i=2;i<d;i++){
+ if(d%i==0){
+ c++;
+ }
+ }
+ if(c==0){
+ cou++; 
+ System.out.println(d);
+ } 
+ d++; 
+ c=0;
+ }
+ return cou;
+ }
+ public static void main(String[] args){
+ task5 obj = new task5();
+ System.out.println("Count of primes between 1 and 20: " + obj.countPrimesInRange(1, 20));
+System.out.println("Count of primes between10 and 30: " + obj.countPrimesInRange(10,30));
 }
-
+}
